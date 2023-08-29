b93d18da0f9cef0b5893c5f481d90aaae4943d2a
ehdgjslee <wkdak2164@gmail.com>
2023-08-29 19:57:31 +0900

    feat/#5 불필요한 import 제거

--- src/main/java/com/lk/chat/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/com/lk/chat/chatroom/repository/ChatRoomRepository.java
@@ -1,7 +1,6 @@
 package com.lk.chat.chatroom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
